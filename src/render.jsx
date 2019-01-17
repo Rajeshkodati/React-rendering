1a9278347b61c73b1c1f1300e17eacd093a65a1c
rajesh <rajesh.kodati@gmail.com>
2019-01-17 19:49:24 +0530

    firstcommit

---
+++ src/render.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const RenderCon = (props) => {
+    return(
+        <div>
+          <p>{props.name}And {props.age}</p>
+
+        </div>
+    );
+
+}
+
+export default RenderCon;
